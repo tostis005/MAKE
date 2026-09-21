7ed7d5a7374085b0968a3b67d7232887f3a38f49
tostis005 <tostis005@gmail.com>
2026-09-21 23:00:34 +0200

    Show larger related product cards

--- functions.php
+++ functions.php
@@ -437,6 +437,13 @@ function make_single_product_reassurance(): void {
 add_filter( 'loop_shop_columns', static fn(): int => 4, 20 );
 add_filter( 'loop_shop_per_page', static fn(): int => 16, 20 );
 
+function make_related_product_layout( array $args ): array {
+    $args['posts_per_page'] = 3;
+    $args['columns'] = 3;
+    return $args;
+}
+add_filter( 'woocommerce_output_related_products_args', 'make_related_product_layout', 20 );
+
 function make_loop_product_classes( array $classes, $product ): array {
     if ( is_a( $product, 'WC_Product' ) ) { $classes[] = 'make-pattern-card'; }
     return $classes;
