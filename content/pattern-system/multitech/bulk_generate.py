40c4f73314e269ae3eec20a2873d8402bfcc2849
tostis005 <tostis005@gmail.com>
2026-09-24 01:13:27 +0200

    Fix Pop Art C2C and tapestry cover assignment

--- content/pattern-system/multitech/bulk_generate.py
+++ content/pattern-system/multitech/bulk_generate.py
@@ -228,7 +228,7 @@ def render_one(task):
     # replace pattern-data block
     template=re.sub(r'(<script id="template-pattern-data" type="application/json">)(.*?)(</script>)',
                     lambda m:m.group(1)+json.dumps(data,ensure_ascii=False,separators=(',',':'))+m.group(3),template,count=1,flags=re.S)
-    cover=ENGINE_ASSETS/f"cover-{ {'CS':'cross-stitch','C2C':'crochet','TC':'c2c-crochet','LH':'rug'}[suffix] }.webp"
+    cover=ENGINE_ASSETS/f"cover-{ {'CS':'cross-stitch','C2C':'c2c-crochet','TC':'crochet','LH':'rug'}[suffix] }.webp"
     floral=ENGINE_ASSETS/'floral.png'
     assets={'floral':data_uri(floral),'cover_image':data_uri(cover)}
     template=re.sub(r'(<script id="template-assets" type="application/json">)(.*?)(</script>)',
